59fcf722d75018dff63a8c1bc849c0af1ad319b0
Njirainianthony <njirainianthony@gmail.com>
2024-09-25 14:26:12 +0300

    commit

--- app/src/main/java/com/example/antoproject/navigation/Routes.kt
+++ app/src/main/java/com/example/antoproject/navigation/Routes.kt
@@ -30,6 +30,4 @@ const val ADD_HOSPITAL_URL="addhospital"
 const val VIEW_HOSPITAL_URL="viewhospital"
 const val VIEW_HOSPITAL2_URL="viewhospital2"
 
-const val ADD_DONATION_URL="adddonation"
-const val VIEW_DONATION_URL="viewdonation"
 
